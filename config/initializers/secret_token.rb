24de1190c590f18a7300c1e767f248f9a8bc4d0a
Ezequiel Aguilar-Gonzalez <ezequiel.aguilarg@gmail.com>
2012-12-06 11:24:25 -0500

    Subo la estructura del proyecto al repositorio de Github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tormenta::Application.config.secret_token = '33f1078f7e8562bb51f82966d89b0cef2b1d804fe340024b784ae079ca98e6b4f9bb1c02b0c8db3c714c80775731f1fa56b1cbad109130fd7eec12363ab3e015'
